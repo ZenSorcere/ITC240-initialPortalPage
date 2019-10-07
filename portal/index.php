f94f54239fa0af436a7d2ed7aefa0481dc11354b
Michael Gilson <49133101+ZenSorcere@users.noreply.github.com>
2019-10-06 20:52:06 -0700

    initial portal pages with css and images

---
+++ portal/index.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Portal Page - ITC240</title>
+<link href="css/styles.css" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+    <header >
+        <h1>Mike Gilson: ITC 240 Fall 2019 Portal Site</h1>
+        <!-- div class="hero-image">
+             <div class="hero-text">
+                <h1>ITC 240 Portal Page</h1>
+                <p>Mike Gilson, Fall 2019</p>
+            </div>  -->
+            <!-- <img  src = "images/spaceArt_1024banner.jpg" alt="sky and space image"> 
+        </div> -->
+    </header>
+    <!-- <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="daily.php">Daily</a></li>
+            <li><a href="customers.php">Customers</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav> -->
+    <div id="wrapper">
+    <!-- <div id="banner">
+        
+    </div> -->
+    
+    <main>
+    <img class="desktop" src="images/me.jpg" alt="Photo of Seahawk from the SheRa Netflix cartoon"/>
+
+    <h2 class="subheader">About Mike!</h2>
+       
+       <p>Welcome to my ITC 240 Portal Website! This site is for my php class as part of my second quarter in the Web Development Certificate program at Seattle Central College.</p>
+
+       <p>My background started in Game Quality Assurance Testing. I dipped my toe into those waters through Volt/VMC, where I did game certification testing for the original Xbox before it was released. However, this was right after the Dotcom Fallout of 2000, so I found myself working along-side people who were new as well as those who had been doing testing for several years. This didnt bode well for my future in the field. When my contract ended, I moved to other career paths, until a few years later where I was hired at a company called Screenlife, LLC (most famously known for the "Scene It?" Trivia board games). I did DVD boardgame testing for them for 7 years before the company was liquidated.</p>
+
+       <p>Just before Screenlife ended, I applied for and was hired at Nordstrom as a Test Lead for their Interactive Development team.  During the 5 years I was at Nordstrom, I built the test team up from one tester and myself doing content verification, to an 8 person team doing feature development testing as part of a DevOps team, creating new features for the Nordstrom.com website. By the time a re-org disolved our team, I had become the Test Program Manager. </p>
+       
+       <p>After some time unemployed and a couple years juggling cell tower construction drawings for a telecom company as a Project Manager, I decided it was time to go back to school and pursue coding skills, which landed me at Seattle Central's WebDev Cert program. </p>
+
+       <h3>Extracurriculars</h3>
+
+       <p>When not pursuing day job or higher education elements, I am heavily involved in Seattle's local theater community. It pretty much takes up all my free time, between performing in them and regularly seeing over 100 performances a year. In addition to acting in several shows over the last decade, I've also found an artistic home at <a href="http://www.annextheatre.org/" target="_blank" title="Annex Theatre">Annex Theatre</a>, where I've been on the volunteer staff as the Bar Manager since 2010.</p>
+
+
+       <p>I also perform as a regular cast member in <i>The Panel Jumper</i>, a webseries about comic book history and lore. I play the oft-beleaguered comic book store manager, Manager Mike.  You can check out Panel Jumper episodes at <a href="http://thepaneljumper.com/home/" target="_blank" title="The Panel Jumper webseries"><strong>www.thepaneljumper.com</strong></a>.</p>
+    </main>
+
+    <aside>
+        <h3>Assignments</h3>
+            <ul>
+                <li>MAMP install/setup</li>
+                <li>Github Account</li>
+                <li>Web Applications: LinkedIn Learning videos</li>
+                <li>Portal Site Creation (due 10/4)</li>
+                <li>PHP Fundamentals: LinkedIn Learning videos (due 10/4)</li>
+           </ul>
+           <p></p>
+        <h3>Weekly Class Exercises</h3>
+            <ul>
+                <li><a href ="https://docs.google.com/document/d/1mk0Vb1bNGPO6lqGCRA60tvRdk5Gfl-gfBq8SDp0Nz4s/edit?usp=sharing" target="_blank" title="Week 2 Class Exercises">Week 2</a></li>
+                <li>Week 3</li>
+                <li>Week 4</li>
+           </ul>
+    </aside>
+    
+    </div> <!-- end wrapper -->
+    <footer> <!-- footer outside wrapper so it can match Header width -->
+    <div class="content-wrap"> <!--wrap footer list so it stays aligned w/ id wrapper -->
+        <ul>
+            <li>Copyright 2015 - <?php echo date('Y'); ?></li>
+            <li>All Rights Reserved</li>
+            <li><a href="">Terms of Use</a></li>
+            <li><a href="">Web Design by Mike</a></li>
+            <li><a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a></li>
+            <li><a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></li>
+        </ul>
+    </div> <!-- end "content-wrap" div -->
+    </footer>
+    
+    
+</body>
+</html>
